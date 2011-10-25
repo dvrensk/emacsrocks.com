d121ca0934d06d99d6bc5bad18f3de48f1a937d7
Magnar Sveen <magnars@gmail.com>
2011-10-25 20:09:56 +0200

    Add short command synopsis to episodes.

--- lib/episodes.rb
+++ lib/episodes.rb
@@ -7,10 +7,10 @@ class Episodes
 
   def self.find_all
     [
-     Episode.new(:number => "01", :name => "From var to this", :size => "21mb", :youtube => "O0UgY-DmFbU"),
-     Episode.new(:number => "02", :name => "A vimgolf eagle", :size => "30mb", :youtube => "dE2haYu0co8"),
-     Episode.new(:number => "03", :name => "A vimgolf albatross", :size => "14mb", :youtube => "ePIHUfFz8-c"),
-     Episode.new(:number => "04", :name => "A rebind controversy", :size => "14mb", :youtube => "OA0AjzBgWU4")
+     Episode.new(:number => "01", :name => "From var to this",     :size => "21mb", :youtube => "O0UgY-DmFbU", :commands => ["string-rectangle", "isearch-forward"]),
+     Episode.new(:number => "02", :name => "A vimgolf eagle",      :size => "30mb", :youtube => "dE2haYu0co8", :commands => ["kmacro-start-macro", "digit-argument"]),
+     Episode.new(:number => "03", :name => "A vimgolf albatross",  :size => "14mb", :youtube => "ePIHUfFz8-c", :commands => ["transpose-lines", "transpose-chars", "kmacro-start-macro", "digit-argument"]),
+     Episode.new(:number => "04", :name => "A rebind controversy", :size => "14mb", :youtube => "OA0AjzBgWU4", :commands => ["iy-go-to-char", "downcase-word", "transpose-words"])
     ]
   end
 
@@ -20,10 +20,10 @@ class Episodes
 end
 
 class Episode
-  attr_reader :number, :youtube, :size, :name
+  attr_reader :number, :youtube, :size, :name, :commands
 
   def initialize(params)
-    @number, @youtube, @size, @name = params[:number], params[:youtube], params[:size], params[:name]
+    @number, @youtube, @size, @name, @commands = params[:number], params[:youtube], params[:size], params[:name], params[:commands]
   end
 
   def next
