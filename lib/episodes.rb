e30d3959129a32d53f9f8b2166fd20f5a2617d55
Magnar Sveen <magnars@gmail.com>
2011-11-22 21:55:23 +0100

    Episode 08

--- lib/episodes.rb
+++ lib/episodes.rb
@@ -13,7 +13,8 @@ class Episodes
      Episode.new(:number => "04", :name => "A rebind controversy", :size => "14mb", :youtube => "OA0AjzBgWU4", :commands => ["iy-go-to-char", "downcase-word", "transpose-words"]),
      Episode.new(:number => "05", :name => "Macros in style",      :size => "12mb", :youtube => "o1jJS_aibPA", :commands => ["forward-list", "kmacro-start-macro"]),
      Episode.new(:number => "06", :name => "Yeah! Snippets!",      :size => "8mb",  :youtube => "1W66B3CHaUo", :commands => ["yas-expand"]),
-     Episode.new(:number => "07", :name => "Mind. Exploded.",      :size => "14mb", :youtube => "NXTf8_Arl1w", :commands => ["key-chord-define-global"])
+     Episode.new(:number => "07", :name => "Mind. Exploded.",      :size => "14mb", :youtube => "NXTf8_Arl1w", :commands => ["key-chord-define-global"]),
+     Episode.new(:number => "08", :name => "mark-multiple",        :size => "12mb", :youtube => "r2o9HYi7DOY", :commands => ["mark-next-like-this", "rename-sgml-tag", "inline-string-rectangle"])
     ]
   end
 
