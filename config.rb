7a5c0de50fcfcb341539454fae945d990318c4c2
Michael Kohl <citizen428@gmail.com>
2012-05-21 00:39:31 +0200

    First version of Atom feed

--- config.rb
+++ config.rb
@@ -78,6 +78,12 @@ end
 
 page "/", :proxy => "/episodes.html", :ignore => true
 
+require "./lib/atom"
+
+page "/atom.xml", :proxy => "/atom.xml", :layout => "none", :ignore => true do
+  @feed = Atom.build
+end
+
 # Build-specific configuration
 configure :build do
   activate :minify_css
