5fe3d244aefb2f2eb04bab0b1138ffdecd5f6a1c
Magnar Sveen <magnars@gmail.com>
2012-05-23 09:26:48 +0200

    Add dates to episodes for RSS-feed.

--- lib/atom.rb
+++ lib/atom.rb
@@ -10,13 +10,13 @@ module Atom
     atom.instruct!
     atom.feed "xmlns" => "http://www.w3.org/2005/Atom" do |variable|
       atom.id "urn:emacsrocks-com:feed"
-      atom.updated Time.now.utc.iso8601(0)
+      atom.updated Episodes.all[-1].time.iso8601(0)
       atom.title "Emacs Rocks!", :type => "text"
       atom.link :rel => "self", :href => "#{base_uri}/atom.xml"
       Episodes.all[-10..-1].each do |episode|
         atom.entry do
           atom.title "#{episode.number}: #{episode.name}"
-          atom.updated Time.now.utc.iso8601(0)
+          atom.updated episode.time.iso8601(0)
           atom.author { atom.name "Magnar Sveen" }
           atom.link "href" => "#{base_uri}/#{episode.link}"
           atom.id "urn:emacsrocks-com:feed:episode:#{episode.number}"
--- lib/episodes.rb
+++ lib/episodes.rb
@@ -7,16 +7,16 @@ module Episodes
 
   def self.find_all
     [
-     Episode.new(:number => "01", :name => "From var to this",     :size => "21mb", :youtube => "O0UgY-DmFbU", :commands => ["string-rectangle", "isearch-forward"]),
-     Episode.new(:number => "02", :name => "A vimgolf eagle",      :size => "30mb", :youtube => "dE2haYu0co8", :commands => ["kmacro-start-macro", "digit-argument"]),
-     Episode.new(:number => "03", :name => "A vimgolf albatross",  :size => "14mb", :youtube => "ePIHUfFz8-c", :commands => ["transpose-lines", "transpose-chars", "kmacro-start-macro", "digit-argument"]),
-     Episode.new(:number => "04", :name => "A rebind controversy", :size => "14mb", :youtube => "OA0AjzBgWU4", :commands => ["iy-go-to-char", "downcase-word", "transpose-words"]),
-     Episode.new(:number => "05", :name => "Macros in style",      :size => "12mb", :youtube => "o1jJS_aibPA", :commands => ["forward-list", "kmacro-start-macro"]),
-     Episode.new(:number => "06", :name => "Yeah! Snippets!",      :size => "8mb",  :youtube => "1W66B3CHaUo", :commands => ["yas-expand"]),
-     Episode.new(:number => "07", :name => "Mind. Exploded.",      :size => "14mb", :youtube => "NXTf8_Arl1w", :commands => ["key-chord-define-global"]),
-     Episode.new(:number => "08", :name => "mark-multiple",        :size => "12mb", :youtube => "r2o9HYi7DOY", :commands => ["mark-next-like-this", "rename-sgml-tag", "inline-string-rectangle"]),
-     Episode.new(:number => "09", :name => "expand-region",        :size => "16mb", :youtube => "_RvHz3vJ3kA", :commands => ["expand-region"]),
-     Episode.new(:number => "10", :name => "Jumping around",       :size => "23mb", :youtube => "UZkpmegySnc", :commands => ["ace-jump-mode", "ido-imenu"])
+     Episode.new(:number => "01", :name => "From var to this",     :time => Time.utc(2011, "oct", 21), :size => "21mb", :youtube => "O0UgY-DmFbU", :commands => ["string-rectangle", "isearch-forward"]),
+     Episode.new(:number => "02", :name => "A vimgolf eagle",      :time => Time.utc(2011, "oct", 21), :size => "30mb", :youtube => "dE2haYu0co8", :commands => ["kmacro-start-macro", "digit-argument"]),
+     Episode.new(:number => "03", :name => "A vimgolf albatross",  :time => Time.utc(2011, "oct", 22), :size => "14mb", :youtube => "ePIHUfFz8-c", :commands => ["transpose-lines", "transpose-chars", "kmacro-start-macro", "digit-argument"]),
+     Episode.new(:number => "04", :name => "A rebind controversy", :time => Time.utc(2011, "oct", 24), :size => "14mb", :youtube => "OA0AjzBgWU4", :commands => ["iy-go-to-char", "downcase-word", "transpose-words"]),
+     Episode.new(:number => "05", :name => "Macros in style",      :time => Time.utc(2011, "oct", 26), :size => "12mb", :youtube => "o1jJS_aibPA", :commands => ["forward-list", "kmacro-start-macro"]),
+     Episode.new(:number => "06", :name => "Yeah! Snippets!",      :time => Time.utc(2011, "nov", 2),  :size => "8mb",  :youtube => "1W66B3CHaUo", :commands => ["yas-expand"]),
+     Episode.new(:number => "07", :name => "Mind. Exploded.",      :time => Time.utc(2011, "nov", 9),  :size => "14mb", :youtube => "NXTf8_Arl1w", :commands => ["key-chord-define-global"]),
+     Episode.new(:number => "08", :name => "mark-multiple",        :time => Time.utc(2011, "nov", 22), :size => "12mb", :youtube => "r2o9HYi7DOY", :commands => ["mark-next-like-this", "rename-sgml-tag", "inline-string-rectangle"]),
+     Episode.new(:number => "09", :name => "expand-region",        :time => Time.utc(2012, "jan", 25), :size => "16mb", :youtube => "_RvHz3vJ3kA", :commands => ["expand-region"]),
+     Episode.new(:number => "10", :name => "Jumping around",       :time => Time.utc(2012, "apr", 10), :size => "23mb", :youtube => "UZkpmegySnc", :commands => ["ace-jump-mode", "ido-imenu"])
     ]
   end
 
@@ -26,10 +26,10 @@ module Episodes
 end
 
 class Episode
-  attr_reader :number, :youtube, :size, :name, :commands
+  attr_reader :number, :youtube, :size, :name, :commands, :time
 
   def initialize(params)
-    @number, @youtube, @size, @name, @commands = params[:number], params[:youtube], params[:size], params[:name], params[:commands]
+    @number, @youtube, @size, @name, @commands, @time = params[:number], params[:youtube], params[:size], params[:name], params[:commands], params[:time]
   end
 
   def next
