2c9c1234f9987f3544da3e1a6c2aebbc6a18eb51
Magnar Sveen <magnars@gmail.com>
2012-07-01 20:23:49 +0200

    Episode 05 of Extending Emacs Rocks

--- lib/episodes.rb
+++ lib/episodes.rb
@@ -33,7 +33,8 @@ module Episodes
      ExtendingEpisode.new(:number => "01", :time => Time.utc(2012, "jun", 17), :size => "97mb",  :youtube => "5axK-VUKJnk", :commits => [{ :hash => "56e0b6e6", :msg => "Initial commit." }, { :hash => "e123fabe", :msg => "Tests up and running." }]),
      ExtendingEpisode.new(:number => "02", :time => Time.utc(2012, "jun", 17), :size => "129mb", :youtube => "Zxt-c_N82_w", :commits => [{ :hash => "b4a79c4e", :msg => "Implemented toggle-deffered." }]),
      ExtendingEpisode.new(:number => "03", :time => Time.utc(2012, "jun", 20), :size => "205mb", :youtube => "Dgcx5blog6s", :commits => [{ :hash => "f3fe2dd2", :msg => "Support hipster quotes." }, { :hash => "e83c3192", :msg => "Create a buster minor-mode." }]),
-     ExtendingEpisode.new(:number => "04", :time => Time.utc(2012, "jun", 27), :size => "197mb", :youtube => "zI4KfUPRU5s", :commits => [{ :hash => "6fb1cf17", :msg => "Toggle without moving point." }, { :hash => "029eab32", :msg => "Autotest." }, { :hash => "bf7fd38a", :msg => "Remove bogus prose." }])
+     ExtendingEpisode.new(:number => "04", :time => Time.utc(2012, "jun", 27), :size => "197mb", :youtube => "zI4KfUPRU5s", :commits => [{ :hash => "6fb1cf17", :msg => "Toggle without moving point." }, { :hash => "029eab32", :msg => "Autotest." }, { :hash => "bf7fd38a", :msg => "Remove bogus prose." }]),
+     ExtendingEpisode.new(:number => "05", :time => Time.utc(2012, "jul", 1),  :size => "173mb", :youtube => "3-7nLXGf1Xg", :commits => [{ :hash => "f5af5d31", :msg => "Toggle focus rocket." }])
     ]
   end
 
